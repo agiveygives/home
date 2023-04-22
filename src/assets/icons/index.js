719db28c92dfc56145615c11c8c2c21cf8c7566e
agiveygives <justincase25xxv@gmail.com>
2023-04-22 11:40:20 -0500

    styling and more links

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,7 @@
+export { default as awsIcon } from './aws.png';
+export { default as chatgptIcon } from './chatgpt.png';
+export { default as firebaseIcon } from './firebase.png';
 export { default as githubIcon } from './github.png';
+export { default as netlifyIcon } from './netlify.svg';
 export { default as vercelIcon } from './vercel.png';
 
--- src/pages/Desktops/Personal/links.js
+++ src/pages/Desktops/Personal/links.js
@@ -1,5 +1,9 @@
 import {
+  awsIcon,
+  chatgptIcon,
+  firebaseIcon,
   githubIcon,
+  netlifyIcon,
   vercelIcon,
 } from '../../../assets/icons';
 
@@ -8,13 +12,37 @@ const desktopLinks = [
     label: 'Github',
     href: 'https://github.com',
     alt: 'Github logo',
-    icon: githubIcon
+    icon: githubIcon,
+  },
+  {
+    label: 'ChatGPT',
+    href: 'https://chat.openai.com/',
+    alt: 'Open AI Chat GPT',
+    icon: chatgptIcon,
+  },
+  {
+    label: 'Firebase',
+    href: 'https://console.firebase.google.com/u/1/',
+    alt: 'Google Firebase',
+    icon: firebaseIcon,
+  },
+  {
+    label: 'AWS Console',
+    href: 'https://aws.amazon.com/console/',
+    alt: 'AWS logo',
+    icon: awsIcon,
   },
   {
     label: 'Vercel',
     href: 'https://vercel.com/dashboard',
     alt: 'Vercel logo',
-    icon: vercelIcon
+    icon: vercelIcon,
+  },
+  {
+    label: 'Netlify',
+    href: 'https://app.netlify.com/teams/agiveygives/overview',
+    alt: 'Netlify logo',
+    icon: netlifyIcon,
   },
 ];
 
